b07d24e63b347a6b057d10edeba7551ec4759246
OverlineJunior <franciscosebastianyjunior@hotmail.com>
2022-04-03 02:04:30 -0300

    stufF

--- src/ServerStorage/Math.lua
+++ src/ServerStorage/Math.lua
@@ -2,10 +2,27 @@
     @class Math
 
     This is the Math class.
+    @server
 ]=]
 local Math = {}
 
+--[=[
+    PI and stuff.
 
+    @prop PI number
+    @within Math
+]=]
+Math.PI = math.pi
+
+
+--[=[
+    This is a function that adds one to a number.
+
+    @param n number -- Number to add one
+    @return number -- Returns `n` plus one
+    @error "Stuff happened" -- Whens when idk.
+    @plugin
+]=]
 function Math.PlusOne(n: number): number
     return n + 1
 end
